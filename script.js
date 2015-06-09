cdf08bed598139d1bedb61ed97256c3ec41716fd
katherinepamina <katherinelin2016@u.northwestern.edu>
2015-06-08 19:25:40 -0500

    lala..

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
-	// jQuery methods go here
 	$.fn.scrollView = function () {
 	    return this.each(function () {
 	        $('html, body').animate({
-	            scrollTop: $(this).offset().top
+	            scrollTop: $(this).offset().top - 150
 	        }, 1000);
     	});
 	}
